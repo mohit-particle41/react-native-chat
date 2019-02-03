9440d3fea7796df51bbf75406b218963ad23cad8
Mohit Choudhary <mohit@particle41.com>
2019-02-03 11:32:19 +0530

    initital

--- ios/RNReactNativeChat.m
+++ ios/RNReactNativeChat.m
@@ -9,5 +9,12 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(pingMe:(RCTResponseSenderBlock)callback)
+{
+    NSString *str = @"Testing Chat";
+    RCTLogInfo(@"Pretending to create an event Mohit@ at Sam@");
+    callback(@[str]);
+}
+
 @end
   
