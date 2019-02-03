4c86fd5b23f5c393e4afb90a9ab2967258376366
Mohit Choudhary <mohit@particle41.com>
2019-02-03 13:20:31 +0530

    d

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 import React, {Component} from 'react'
 import { NativeModules, View, Text } from 'react-native';
 
-const { RNReactNativeChat } = NativeModules;
 
+var nativeChat = NativeModules.RNReactNativeChat
 export default class TcdChat extends Component {
     render() {
         // RNReactNativeChat.pingMe(str => {
         //     console.log("5555555555",str)
         // })
-        console.log("checking native component",RNReactNativeChat)
+        console.log("checking native component",nativeChat)
         return (
             <View>
             <Text>Inside Chat Lib</Text>
