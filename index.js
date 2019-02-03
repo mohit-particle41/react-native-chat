cff30dbcf580831308d4ee8a6b3caef468affdbf
Mohit Choudhary <mohit@particle41.com>
2019-02-03 13:06:46 +0530

    dfs

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const { RNReactNativeChat } = NativeModules;
 
 export default class TcdChat extends Component {
     render() {
-        RNReactNativeChat.pingMe(str => {
-            console.log("5555555555",str)
-        })
+        // RNReactNativeChat.pingMe(str => {
+        //     console.log("5555555555",str)
+        // })
+        console.log("checking native component",RNReactNativeChat)
         return (
             <View>
             <Text>Inside Chat Lib</Text>
